e37506354fc72439cd1cf31b76a0724b00b71408
Pony <74635349+Epic-Deno@users.noreply.github.com>
2024-12-27 11:05:43 +0800

    样式路径

--- new-template/src/main.ts
+++ new-template/src/main.ts
@@ -3,14 +3,14 @@
  * @Author: zhang zhen
  * @Date: 2024-12-27 10:18:00
  * @LastEditors: zhang zhen
- * @LastEditTime: 2024-12-27 11:04:08
+ * @LastEditTime: 2024-12-27 11:05:28
  * @FilePath: /new-template/src/main.ts
  */
 import { createApp } from 'vue'
 import './style.css'
 import Antd from 'ant-design-vue';
 import App from './App.vue'
-import 'ant-design-vue/reset.css';
+import 'ant-design-vue/dist/reset.css';
 import router from './router/index';
 
 

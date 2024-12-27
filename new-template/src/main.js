41e3bcc1e7282df9099e98cf268edb88323ed836
Pony <74635349+Epic-Deno@users.noreply.github.com>
2024-12-27 09:56:42 +0800

    引入antd

--- new-template/src/main.js
+++ new-template/src/main.js
@@ -1,5 +1,17 @@
+/*
+ * @Description:初始化
+ * @Author: zhang zhen
+ * @Date: 2024-12-27 09:49:44
+ * @LastEditors: zhang zhen
+ * @LastEditTime: 2024-12-27 09:56:18
+ * @FilePath: /new-template/src/main.js
+ */
 import { createApp } from 'vue'
 import './style.css'
+import Antd from 'ant-design-vue';
 import App from './App.vue'
+import 'ant-design-vue/reset.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(Antd).mount('#app')

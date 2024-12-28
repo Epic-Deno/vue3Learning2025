f32ce2ea2c5493a3a984ada6a14c9fdbf8948fd9
Pony <74635349+Epic-Deno@users.noreply.github.com>
2024-12-28 16:34:19 +0800

    请求

--- new-template/src/utils/request.ts
+++ new-template/src/utils/request.ts
@@ -3,6 +3,7 @@
  * @Author: zhang zhen
  * @Date: 2024-12-27 10:02:45
  * @LastEditors: zhang zhen
- * @LastEditTime: 2024-12-27 10:02:46
+ * @LastEditTime: 2024-12-28 16:34:11
  * @FilePath: /new-template/src/utils/request.ts
  */
+import axios from 'axios';

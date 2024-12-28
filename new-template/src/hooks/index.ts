627510d8dea8c8c65f0b927ddfd35355e6a5818f
Pony <74635349+Epic-Deno@users.noreply.github.com>
2024-12-28 16:33:58 +0800

    changes

---
+++ new-template/src/hooks/index.ts
@@ -0,0 +1,8 @@
+/*
+ * @Description: 钩子函数
+ * @Author: zhang zhen
+ * @Date: 2024-12-28 16:33:43
+ * @LastEditors: zhang zhen
+ * @LastEditTime: 2024-12-28 16:33:44
+ * @FilePath: /new-template/src/hooks/index.ts
+ */

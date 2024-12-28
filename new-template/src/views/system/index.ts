9efb4e8055e5274eed0a1f33c0878cb2116e3c12
Pony <74635349+Epic-Deno@users.noreply.github.com>
2024-12-28 16:35:17 +0800

    修改

---
+++ new-template/src/views/system/index.ts
@@ -0,0 +1,8 @@
+/*
+ * @Description: 系统管理
+ * @Author: zhang zhen
+ * @Date: 2024-12-28 16:35:02
+ * @LastEditors: zhang zhen
+ * @LastEditTime: 2024-12-28 16:35:03
+ * @FilePath: /new-template/src/views/system/index.ts
+ */

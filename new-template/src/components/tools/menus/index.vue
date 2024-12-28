4c8a84528e2d3483f7edf77f7b54a421dfad0e33
Pony <74635349+Epic-Deno@users.noreply.github.com>
2024-12-28 16:22:30 +0800

    changes

--- new-template/src/components/tools/menus/index.vue
+++ new-template/src/components/tools/menus/index.vue
@@ -3,14 +3,18 @@
  * @Author: zhang zhen
  * @Date: 2024-12-28 16:04:31
  * @LastEditors: zhang zhen
- * @LastEditTime: 2024-12-28 16:10:06
+ * @LastEditTime: 2024-12-28 16:21:38
  * @FilePath: /new-template/src/components/tools/menus/index.vue
 -->
 <template>
-    <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+    <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline" @select="handleClickMenus">
         <a-menu-item key="dashborad">
             <HomeOutlined />
             <span>首页</span>
+        </a-menu-item>
+        <a-menu-item key="test">
+            <ExperimentOutlined />
+            <span>测试</span>
         </a-menu-item>
     </a-menu>
 </template>
@@ -18,12 +22,18 @@
 <script setup lang="ts">
     import { ref } from 'vue';
     import {
-        HomeOutlined
+        HomeOutlined,
+        ExperimentOutlined
     } from '@ant-design/icons-vue';
-    import { useRoute } from "vue-router";
+    import { useRoute, useRouter } from "vue-router";
     const { name } = useRoute();
+    const { push } = useRouter();
     const selectedKeys = ref<string[]>([name]);
 
+    const handleClickMenus = (e) => {
+        const { key } = e;
+        push(key)
+    }
 </script>
 
 <style lang="less" scoped></style>

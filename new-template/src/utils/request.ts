e1b4b626a5ad779ff92328dac72954d55825b8cf
Pony <74635349+Epic-Deno@users.noreply.github.com>
2024-12-29 11:34:27 +0800

    修改

--- new-template/src/utils/request.ts
+++ new-template/src/utils/request.ts
@@ -3,13 +3,19 @@
  * @Author: zhang zhen
  * @Date: 2024-12-27 10:02:45
  * @LastEditors: zhang zhen
- * @LastEditTime: 2024-12-29 11:27:24
+ * @LastEditTime: 2024-12-29 11:34:21
  * @FilePath: /new-template/src/utils/request.ts
  */
 import axios from 'axios';
 import { RequestObject } from '@/types/request';
+import type { AxiosError, AxiosRequestConfig } from 'axios'
 
-const basicConfig: RequestObject = {
+const basicConfig: AxiosRequestConfig = {
     baseURL: import.meta.env.VITE_APP_BASE_API,
     timeout: 10000
 };
+
+
+const service:  = axios.create({
+    ...basicConfig
+  })

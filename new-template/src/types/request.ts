1bb3b2561b1c2b77f8ac4635a349cd3849774f07
Pony <74635349+Epic-Deno@users.noreply.github.com>
2024-12-29 11:27:39 +0800

    请求方式的封装

---
+++ new-template/src/types/request.ts
@@ -0,0 +1,19 @@
+/*
+ * @Description: 请求的枚举
+ * @Author: zhang zhen
+ * @Date: 2024-12-29 11:25:47
+ * @LastEditors: zhang zhen
+ * @LastEditTime: 2024-12-29 11:25:48
+ * @FilePath: /new-template/src/types/request.ts
+ */
+
+interface RequestObject {
+    baseURL: string;
+    timeout: number;
+}
+
+
+
+export {
+    RequestObject
+}
--- new-template/src/utils/request.ts
+++ new-template/src/utils/request.ts
@@ -3,7 +3,13 @@
  * @Author: zhang zhen
  * @Date: 2024-12-27 10:02:45
  * @LastEditors: zhang zhen
- * @LastEditTime: 2024-12-28 16:34:11
+ * @LastEditTime: 2024-12-29 11:27:24
  * @FilePath: /new-template/src/utils/request.ts
  */
 import axios from 'axios';
+import { RequestObject } from '@/types/request';
+
+const basicConfig: RequestObject = {
+    baseURL: import.meta.env.VITE_APP_BASE_API,
+    timeout: 10000
+};

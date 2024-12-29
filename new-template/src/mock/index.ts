1e178a0060236da6f5c69a9d85761179ae2c8178
Pony <74635349+Epic-Deno@users.noreply.github.com>
2024-12-29 11:23:53 +0800

    修改

---
+++ new-template/src/mock/index.ts
@@ -0,0 +1,8 @@
+/*
+ * @Description: 模拟数据
+ * @Author: zhang zhen
+ * @Date: 2024-12-29 11:23:38
+ * @LastEditors: zhang zhen
+ * @LastEditTime: 2024-12-29 11:23:39
+ * @FilePath: /new-template/src/mock/index.ts
+ */

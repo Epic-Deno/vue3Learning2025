c82d9c19b9f7dedf76f66798f64f49f45bd55a14
Pony <74635349+Epic-Deno@users.noreply.github.com>
2024-12-30 11:54:10 +0800

    类型

--- new-template/src/utils/request.ts
+++ new-template/src/utils/request.ts
@@ -3,12 +3,12 @@
  * @Author: zhang zhen
  * @Date: 2024-12-27 10:02:45
  * @LastEditors: zhang zhen
- * @LastEditTime: 2024-12-30 11:50:09
+ * @LastEditTime: 2024-12-30 11:53:27
  * @FilePath: /new-template/src/utils/request.ts
  */
 import axios from 'axios';
 import { RequestObject, ResponseData, ErrorResponse } from '@/types/request';
-import type { AxiosError, AxiosRequestConfig, AxiosInstance } from 'axios'
+import type { AxiosError, AxiosRequestConfig, AxiosInstance, AxiosResponse, InternalAxiosRequestConfig } from 'axios'
 
 const basicConfig: AxiosRequestConfig = {
     baseURL: import.meta.env.VITE_APP_BASE_API,
@@ -19,39 +19,51 @@ const basicConfig: AxiosRequestConfig = {
 const service: AxiosInstance = axios.create({
     ...basicConfig
   })
-
-
-  // axios 请求封装
-  // 发起请求之前的拦截器
-service.interceptors.request.use(
-    config => {
-      // 如果有token 就携带tokon
-      const token = window.localStorage.getItem("token");
+   
+  // 请求拦截器
+  service.interceptors.request.use(
+    (config: InternalAxiosRequestConfig) => {
+      // 从 localStorage 获取 token
+      const token = window.localStorage.getItem('token');
       if (token) {
+        // 如果存在 token，则设置到请求头中
         config.headers['Authorization'] = token;
       }
       return config;
     },
-    error => Promise.reject(new Error(error.message || error))
-  );
-  // 响应拦截器
-  service.interceptors.response.use(
-    (response) => {
-      const res = response.data;
-      if (response.status !== 200) {
-        return Promise.reject(new Error(res.message || "Error"));
-      } else {
-        const { code } = res
-        if (code == '401') {
-          window.localStorage.clear();
-          return location.reload();
-        }
-        return res;
-      }
-    },
-    error => {
-      return Promise.reject(new Error(error.message || error));
+    (error: AxiosError) => {
+      // 对请求错误做些什么
+      return Promise.reject(new Error(error.message || 'Request Error'));
     }
   );
-
+   
+  // 响应拦截器
+  service.interceptors.response.use(
+    (response: AxiosResponse) => {
+      const res = response.data;
+      const { status, statusText } = response;
+   
+      // 检查 HTTP 状态码
+      if (status !== 200) {
+        return Promise.reject(new Error(res.message || `HTTP Error ${status}: ${statusText}`));
+      }
+   
+      // 检查业务逻辑状态码
+      const { code } = res as { code: string }; // 假设响应数据中有 code 字段
+      if (code === '401') {
+        // 清除本地存储并重定向（注意：在浏览器环境中使用 window.location.reload()）
+        window.localStorage.clear();
+        window.location.reload();
+        // 注意：这里返回 undefined 因为页面会重新加载，实际上这个返回值不会被用到
+        return undefined as never; // 使用 never 类型表示这个分支不应该有返回值
+      }
+   
+      return res;
+    },
+    (error: AxiosError) => {
+      // 对响应错误做些什么
+      return Promise.reject(new Error(error.message || 'Response Error'));
+    }
+  );
+   
   export default service;

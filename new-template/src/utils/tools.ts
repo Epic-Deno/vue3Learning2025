0b83d25274383b137f82ba65770a6b5922d2d114
Pony <74635349+Epic-Deno@users.noreply.github.com>
2024-12-31 14:57:02 +0800

    初始化

---
+++ new-template/src/utils/tools.ts
@@ -0,0 +1,8 @@
+/*
+ * @Description: 工具类
+ * @Author: zhang zhen
+ * @Date: 2024-12-31 14:56:34
+ * @LastEditors: zhang zhen
+ * @LastEditTime: 2024-12-31 14:56:35
+ * @FilePath: /new-template/src/utils/tools.ts
+ */

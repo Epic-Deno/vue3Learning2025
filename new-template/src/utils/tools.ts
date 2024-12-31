d9f378382b49f8cef260f7e7bd756b878728505c
Pony <74635349+Epic-Deno@users.noreply.github.com>
2024-12-31 14:59:12 +0800

    changes

--- new-template/src/utils/tools.ts
+++ new-template/src/utils/tools.ts
@@ -94,4 +94,12 @@ export function filterObj(obj) {
     }
     return obj;
   }
+  /**
+ * 深度克隆对象、数组
+ * @param obj 被克隆的对象
+ * @return 克隆后的对象
+ */
+export function cloneObject(obj) {
+    return JSON.parse(JSON.stringify(obj))
+  }
   

8b221f3ae989317fdfdec88a836ba52c5aa723f2
Pony <74635349+Epic-Deno@users.noreply.github.com>
2025-01-01 15:46:44 +0800

    修改

---
+++ new-template/src/views/example/table.vue
@@ -0,0 +1,8 @@
+<!--
+ * @Description: 表格例子
+ * @Author: zhang zhen
+ * @Date: 2025-01-01 15:46:23
+ * @LastEditors: zhang zhen
+ * @LastEditTime: 2025-01-01 15:46:25
+ * @FilePath: /new-template/src/views/example/table.vue
+-->

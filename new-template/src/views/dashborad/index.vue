f631d2aefd72a3d6a949bceead92e730f74263d4
Pony <74635349+Epic-Deno@users.noreply.github.com>
2025-01-01 15:41:31 +0800

    changes

--- new-template/src/views/dashborad/index.vue
+++ new-template/src/views/dashborad/index.vue
@@ -3,19 +3,19 @@
  * @Author: zhang zhen
  * @Date: 2024-12-27 11:00:54
  * @LastEditors: zhang zhen
- * @LastEditTime: 2024-12-27 11:00:55
+ * @LastEditTime: 2025-01-01 15:41:23
  * @FilePath: /new-template/src/views/dashborad/index.vue
 -->
 <template>
-    <div>
-1111
-    </div>
+    <a-row :gutter="16">
+        <a-col :span="8">
+            
+        </a-col>
+    </a-row>
 </template>
 
 <script setup lang="ts">
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>

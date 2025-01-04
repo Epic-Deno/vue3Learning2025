33f51e83db48ad32958a55a1175d6651a4c4b71f
Pony <74635349+Epic-Deno@users.noreply.github.com>
2025-01-04 15:23:13 +0800

    抽屉

---
+++ new-template/src/views/example/modules/drawerView.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Description: 抽屉页面
+ * @Author: zhang zhen
+ * @Date: 2025-01-04 15:22:40
+ * @LastEditors: zhang zhen
+ * @LastEditTime: 2025-01-04 15:22:41
+ * @FilePath: /new-template/src/views/example/modules/drawerView.vue
+-->
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="less" scoped>
+
+</style>

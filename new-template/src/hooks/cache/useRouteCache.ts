d31e31c2923a41c859c169dee14c9bf8639e8d7f
Pony <74635349+Epic-Deno@users.noreply.github.com>
2025-01-06 18:08:16 +0800

    changes

---
+++ new-template/src/hooks/cache/useRouteCache.ts
@@ -0,0 +1,8 @@
+/*
+ * @Description: 路由缓存
+ * @Author: zhang zhen
+ * @Date: 2025-01-06 18:08:00
+ * @LastEditors: zhang zhen
+ * @LastEditTime: 2025-01-06 18:08:02
+ * @FilePath: /new-template/src/hooks/cache/useRouteCache.ts
+ */

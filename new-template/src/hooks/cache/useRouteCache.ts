f9c2820b0e8702d4d4ca7bfbbbdbc94212184882
Pony <74635349+Epic-Deno@users.noreply.github.com>
2025-01-07 21:28:58 +0800

    changes

--- new-template/src/hooks/cache/useRouteCache.ts
+++ new-template/src/hooks/cache/useRouteCache.ts
@@ -15,6 +15,21 @@ export default function useRouteCache() {
     const router = useRouter();
     const route = useRoute();
 
+    route.matched.forEach((routeMatch) => {
+      const name = routeMatch.name;
+      if (typeof name !== 'string') return;
+      // 配置了meta.keepAlive的路由组件添加到缓存
+      if (routeMatch.meta.enableCache) {
+          if (!name) {
+            console.warn(`${routeMatch.path} 路由的组件名称name为空`);
+            return;
+          }
+          if (!cachePages.value.includes(name)) {
+            cachePages.value.push(name);
+          }
+        }
+    });
+
       router.beforeEach((to, _from, next) => {
         const name = to.name;
         if (typeof name !== 'string') {

1e01d3f9387b6a600191022e176131debc162853
Pony <74635349+Epic-Deno@users.noreply.github.com>
2025-01-07 21:28:46 +0800

    删除

--- new-template/src/hooks/cache/useRouteCache.ts
+++ new-template/src/hooks/cache/useRouteCache.ts
@@ -3,7 +3,7 @@
  * @Author: zhang zhen
  * @Date: 2025-01-06 18:08:00
  * @LastEditors: zhang zhen
- * @LastEditTime: 2025-01-06 18:10:27
+ * @LastEditTime: 2025-01-07 21:28:37
  * @FilePath: /new-template/src/hooks/cache/useRouteCache.ts
  */
 import { ref } from 'vue';
@@ -14,21 +14,6 @@ export default function useRouteCache() {
 
     const router = useRouter();
     const route = useRoute();
-
-    route.matched.forEach((routeMatch) => {
-        const name = routeMatch.name;
-        if (typeof name !== 'string') return;
-        // 配置了meta.keepAlive的路由组件添加到缓存
-        if (routeMatch.meta.enableCache) {
-            if (!name) {
-              console.warn(`${routeMatch.path} 路由的组件名称name为空`);
-              return;
-            }
-            if (!cachePages.value.includes(name)) {
-              cachePages.value.push(name);
-            }
-          }
-      });
 
       router.beforeEach((to, _from, next) => {
         const name = to.name;
